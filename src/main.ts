3a38095fd2cd88c05afff09eadda247c8db6b3ef
Rostislav <rostyslav.fedis@xsine.co>
2022-04-05 19:33:15 +0300

    add headers

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Accept");
+    next();
+  });
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
