5633a5148da32f9dd32eb32fa0823df1cf1ae8a0
Rostislav <rostyslav.fedis@xsine.co>
2022-04-07 14:07:05 +0300

    changed CORS policy

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
   app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type, Accept");
     next();
   });
+
+  app.enableCors({
+    allowedHeaders:"*",
+    origin: "*"
+  });
+
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
